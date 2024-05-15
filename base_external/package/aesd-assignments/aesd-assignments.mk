0736bee6ccd7b98689dbb62b640a2e60e72b8c34
gpcm <gianpcm@gmail.com>
2024-05-14 20:50:50 -0700

    test successful

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '6241eff'
+AESD_ASSIGNMENTS_VERSION = '0b9c93b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,6 +16,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 # ARM_LIB_PATH=$(${CROSS_COMPILE}gcc -print-sysroot)
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -25,6 +26,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
 	# $(INSTALL) -m 0755 $(@D)/finder-app/Makefile $(TARGET_DIR)/bin
 	# $(INSTALL) -m 0755 $(ARM_LIB_PATH)/lib/ld-linux-aarch64.so.1 $(TARGET_DIR)/lib/
 	# $(INSTALL) -m 0755 $(ARM_LIB_PATH)/lib64/libresolv.so.2 $(TARGET_DIR)/lib64/
