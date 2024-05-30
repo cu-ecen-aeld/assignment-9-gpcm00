4f00d97430d89f1ae88496a18dd13d055adc581a
gpcm <gianpcm@gmail.com>
2024-05-29 23:03:54 -0700

    full-tested

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,34 +6,28 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'ac90ac9'
+AESD_ASSIGNMENTS_VERSION = '958c235'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-gpcm00.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_ASSIGNMENTS_LICENSE = 'Dual BSD/GPL'
+AESD_ASSIGNMENTS_LICENSE_FILES = COPYING
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 # ARM_LIB_PATH=$(${CROSS_COMPILE}gcc -print-sysroot)
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
-	# $(INSTALL) -m 0755 $(@D)/finder-app/Makefile $(TARGET_DIR)/bin
-	# $(INSTALL) -m 0755 $(ARM_LIB_PATH)/lib/ld-linux-aarch64.so.1 $(TARGET_DIR)/lib/
-	# $(INSTALL) -m 0755 $(ARM_LIB_PATH)/lib64/libresolv.so.2 $(TARGET_DIR)/lib64/
-	# $(INSTALL) -m 0755 $(ARM_LIB_PATH)/lib64/libm.so.6 $(TARGET_DIR)/lib64/
-	# $(INSTALL) -m 0755 $(ARM_LIB_PATH)/lib64/libc.so.6 $(TARGET_DIR)/lib64/
-	
+
 endef
 
 $(eval $(generic-package))
